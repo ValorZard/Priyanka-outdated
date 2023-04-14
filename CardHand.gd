ff161b1e63de4a838ca76a4ca7bc0cd5e6d0ebe9
Srayan Jana <valorzard.kiko@outlook.com>
2023-04-15 01:08:59 +0200

    writing down coding ideas

--- CardHand.gd
+++ CardHand.gd
@@ -1,6 +1,11 @@
 extends HBoxContainer
 
 
+# need to figure out how to generate cards, and then automatically set up signals when clicked
+# also probably need a game state singleton or something. Will have to figure it out later.
+# also need to code the grid and connect that to the cards
+# https://www.gdquest.com/tutorial/godot/2d/tactical-rpg-movement/lessons/00.handling-grid-interactions/
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
