cbe4797f5b8938917618183974681980ae105104
Srayan Jana <valorzard.kiko@outlook.com>
2023-04-16 00:06:31 +0200

    figured out how to do connection stuff

--- CardHand.gd
+++ CardHand.gd
@@ -6,11 +6,19 @@ extends HBoxContainer
 # also need to code the grid and connect that to the cards
 # https://www.gdquest.com/tutorial/godot/2d/tactical-rpg-movement/lessons/00.handling-grid-interactions/
 
+var count : int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for card in get_children():
+		if card is Card:
+			card.connect("button_up", _on_Card_Selected)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_Card_Selected():
+	print("hello ", count)
+	count += 1
