51264ef7f402a6f6603cb96a4c56d74c9fc75aea
Srayan Jana <valorzard.kiko@outlook.com>
2023-04-28 10:47:42 -0700

    CHARACTER MOVES TOWARDS CURSOR

--- GameBoard/GameBoard.gd
+++ GameBoard/GameBoard.gd
@@ -4,6 +4,8 @@ extends Node3D
 const RAY_LENGTH = 1000.0
 
 var mouse_button_clicked : bool = false
+var current_cursor_position : Vector3 = Vector3.ZERO
+var character_movement_speed : float = 7.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,27 +19,23 @@ func _input(event):
 	elif event is InputEventMouseMotion:
 		#print("Mouse Motion at: ", event.position)
 		pass
-	# Print the size of the viewport.
-	# print("Viewport Resolution is: ", get_viewport_rect().size)
-	# code taken from https://docs.godotengine.org/en/stable/tutorials/physics/ray-casting.html
-	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		mouse_button_clicked = true
-	else:
-		mouse_button_clicked = false
-	#$Cursor.position = Vector3(-5, 5, -5)
-	#print($Cursor.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if mouse_button_clicked:
+	# code taken from https://docs.godotengine.org/en/stable/tutorials/physics/ray-casting.html
+	if Input.is_action_just_released("left_mouse_click"):
 		# generate the raycast starting and ending points
 		var camera3d = $Camera3D
 		var from = camera3d.project_ray_origin(get_viewport().get_mouse_position())
 		var to = from + camera3d.project_ray_normal(get_viewport().get_mouse_position()) * RAY_LENGTH
-		print("from: ", from, " to: " , to)
+		# print("from: ", from, " to: " , to)
 		# actually create the raycast and check for overlaps
 		var space_state = get_world_3d().direct_space_state
 		var query = PhysicsRayQueryParameters3D.create(from, to)
 		var result : Dictionary = space_state.intersect_ray(query)
 		if result:
 			$Cursor.position = result["position"]
+			current_cursor_position = result["position"]
+	
+	if current_cursor_position != Vector3.ZERO:
+		$CharacterUnit.position = $CharacterUnit.position.move_toward(current_cursor_position, character_movement_speed * delta)
