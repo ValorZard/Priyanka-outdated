bc554d94d69bc1a1ed4aef7aa7c8eb68aa4bb13f
notpresident35 <notpresident35@gmail.com>
2023-05-09 15:46:24 -0700

    minor adjustment

--- UI/CardButton.gd
+++ UI/CardButton.gd
@@ -2,7 +2,7 @@ extends Button
 
 class_name CardButton
 
-var card_data : CardData = preload("res://Cards/CardData.tres")
+@export var card_data : CardData = preload("res://Cards/CardData.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
